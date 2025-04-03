7ec299fe77729ca78a2d1a81753a3a97b2392a21
Isaac Beverly <imbev@protonmail.com>
2025-04-03 15:55:04 -0500

    Print nvidia build log on failure

--- build_scripts_overrides/shared/nvidia.sh
+++ build_scripts_overrides/shared/nvidia.sh
@@ -32,8 +32,9 @@ EOF
 install -Dm0755 /tmp/fake-uname /tmp/bin/uname
 
 NVIDIA_DRIVER_VERSION="$(rpm -q dkms-nvidia --queryformat="%{VERSION}")"
-PATH=/tmp/bin:$PATH dkms --force install -m nvidia -v "${NVIDIA_DRIVER_VERSION}" -k "$QUALIFIED_KERNEL"
-cat "/var/lib/dkms/nvidia/${NVIDIA_DRIVER_VERSION}/build/make.log" || echo "Expected failure"
+PATH=/tmp/bin:$PATH \
+  dkms --force install -m nvidia -v "${NVIDIA_DRIVER_VERSION}" -k "$QUALIFIED_KERNEL" \
+  || cat "/var/lib/dkms/nvidia/${NVIDIA_DRIVER_VERSION}/build/make.log"
 
 cat >/usr/lib/modprobe.d/00-nouveau-blacklist.conf <<EOF
 blacklist nouveau
